916ba1812d0cdf0fa0d9d7caaa287b223ba27dbc
Ryuivan <ryu.ivan@student.umn.ac.id>
2024-11-29 01:09:44 +0700

    feat: main menu ui

---
+++ Assets/AlpacaStudio_Menu_Template/Scripts/Sound_Controller.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class Sound_Controller : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private bool _isMute = false;
+
+    [Header("UI Elements")]
+    public Button soundToggleButton; // Tombol untuk toggle suara
+    public Sprite iconSound; // Ikon untuk unmute
+    public Sprite iconMute; // Ikon untuk mute
+
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        // Cek PlayerPrefs untuk status mute
+        if (PlayerPrefs.HasKey("_Mute"))
+        {
+            int _value = PlayerPrefs.GetInt("_Mute");
+            _isMute = _value == 1;
+        }
+        else
+        {
+            _isMute = false;
+            PlayerPrefs.SetInt("_Mute", 0);
+        }
+
+        // Set audio sesuai status
+        _audioSource.mute = _isMute;
+        UpdateButtonIcon();
+    }
+
+    public void ToggleSound()
+    {
+        _isMute = !_isMute; // Toggle status mute
+        _audioSource.mute = _isMute;
+
+        // Simpan status mute di PlayerPrefs
+        PlayerPrefs.SetInt("_Mute", _isMute ? 1 : 0);
+
+        // Update ikon tombol
+        UpdateButtonIcon();
+    }
+
+    private void UpdateButtonIcon()
+    {
+        if (soundToggleButton != null)
+        {
+            // Ubah ikon tombol berdasarkan status mute
+            soundToggleButton.image.sprite = _isMute ? iconMute : iconSound;
+        }
+    }
+}
